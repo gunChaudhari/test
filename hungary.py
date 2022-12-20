9532fd162c6af21af7c6fc04fcda1b786a76627a
gunChaudhari <gunjan22chaudhari@gmail.com>
2022-12-20 18:32:31 +0530

    fruit

--- hungary.py
+++ hungary.py
@@ -2,5 +2,11 @@ print("I am hungry")
 hungry = input("are you hungry?")
 if hungry == ("yes" or "Yes" or "YES"):
     print("Eat fruits")
+    print("Eat fruits")
+    print("Eat fruits")
+    print("Eat fruits")
 else:
     print("Go to sleep")
+    print("Go to sleep")
+print("Go to sleep")
+
